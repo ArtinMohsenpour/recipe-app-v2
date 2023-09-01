458c262981ea16db0c42a05441769dd6df0d9ea2
ArtinMohsenpour <aria1998htl@gmail.com>
2023-09-01 16:58:39 +0200

    jason fraction error solved

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { Fraction } from 'fracty';
 import View from './View';
 
 class RecipeView extends View {
