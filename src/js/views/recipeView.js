4032a203e2695dc5f4aefe38b9b98cc98c13e66b
ArtinMohsenpour <aria1998htl@gmail.com>
2023-09-01 17:18:15 +0200

    feacty error solved

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import icons from 'url:../../img/icons.svg';
 // import { Fraction } from '.fractional';
-import fracty from "fracty";
+import fracty, {Fracty} from "fracty";
 import View from './View';
 
 class RecipeView extends View {
