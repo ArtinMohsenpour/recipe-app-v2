306d12325be78292a83f0ce6b0c18c4418332dbf
ArtinMohsenpour <aria1998htl@gmail.com>
2023-09-01 16:54:52 +0200

    new function added

--- src/js/controller.js
+++ src/js/controller.js
@@ -139,6 +139,9 @@ const controlAddRecipe = async function (newRecipe) {
 //     addRecipeView.renderError(err.message);
 //   }
 // };
+const newFeature = function () {
+  console.log('welcome to new site');
+};
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -148,5 +151,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
