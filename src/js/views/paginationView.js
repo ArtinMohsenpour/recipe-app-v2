26e576be9cc9ec6862079ceb0ea5a6721c68cba9
ArtinMohsenpour <aria1998htl@gmail.com>
2023-09-01 17:10:21 +0200

    fraction error solved

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -20,7 +20,6 @@ class PaginationView extends View {
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
-    console.log(numPages);
     // page 1, and there are other pages
     if (curPage === 1 && numPages > 1) {
       const markup = `
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,6 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fracty';
+// import { Fraction } from '.fractional';
+import fracty from "fracty";
 import View from './View';
 
 class RecipeView extends View {
@@ -135,7 +136,7 @@ class RecipeView extends View {
                <use href="${icons}#icon-check"></use>
            </svg>
              <div class="recipe__quantity">${
-               ing.quantity ? new Fraction(ing.quantity).toString() : ''
+               ing.quantity ? new fracty(ing.quantity).toString() : ''
              }</div>
              <div class="recipe__description">
                <span class="recipe__unit">${ing.unit}</span>
